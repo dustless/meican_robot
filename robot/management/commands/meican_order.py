bb54867b73337558f497733536a1b12e8da2a98c
wuchenghui <wuchenghui927@126.com>
2019-07-29 18:01:51 +0800

    add log

--- robot/management/commands/meican_order.py
+++ robot/management/commands/meican_order.py
@@ -1,10 +1,12 @@
 # coding: utf-8
+import datetime
+
 from django.core.management.base import BaseCommand
 from robot.task import job
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write("start job...")
+        self.stdout.write("[%s] start job..." % datetime.datetime.now())
         job()
-        self.stdout.write('Successfully done')
+        self.stdout.write('[%s] Successfully done' % datetime.datetime.now())
